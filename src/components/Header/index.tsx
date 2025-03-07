a912eda6c756c56a11118dd09dd0cdb58dc44e9d
Melksedeque Silva <freelancer@melksedeque.com.br>
2025-03-07 14:32:34 -0300

    Criação do componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <h1 className="header__title">Alura Newsletter</h1>
+            <h2>Bem-vindo, usuário!</h2>
+            <input type="checkbox" name="check" id="check" />
+        </header>
+    )
+}
