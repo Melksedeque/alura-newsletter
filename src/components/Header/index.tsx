e4f4c548ec5ebf8470317b629f2aae1c97e5359c
Melksedeque <freelancer@melksedeque.com.br>
2025-03-09 14:38:50 -0300

    Remoção de margin do container pois o container agora tem um padding

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { HeaderProps } from "../../types/Header";
 
 export default function Header({ user }: HeaderProps) {
   return (
-    <header className="header flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center p-5 sm:rounded-xl sm:m-5 ">
+    <header className="header flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center p-5 sm:rounded-xl ">
       <h2 className="text-gray-100">Bem-vindo, {user?.name || "Usuário"}!</h2>
       <h1 className="header__title text-gray-100 text-xl">Alura Newsletter</h1>
       <ToggleTheme />
