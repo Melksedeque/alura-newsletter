d356e3799b563b9dd17a265cda66ee72a857b981
Melksedeque <freelancer@melksedeque.com.br>
2025-03-09 15:12:01 -0300

    Estilo do placeholder

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -13,7 +13,7 @@ export default function Input({
     <input
       type={type}
       name={name}
-      className="bg-white dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full px-5 py-2 outline-none w-full"
+      className="bg-white dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full px-5 py-2 outline-none w-full placeholder:text-gray-400 placeholder:dark:text-gray-600"
       value={value}
       onChange={(e) => onChange(e.target.value)}
       placeholder={placeholder}
