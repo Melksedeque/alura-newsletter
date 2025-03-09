6aacab11b683f5ec47b7eb357ba3cfdf4b9d8177
Melksedeque <freelancer@melksedeque.com.br>
2025-03-08 22:41:32 -0300

    Estilização de lista de artigos

--- src/components/ArticleList/index.tsx
+++ src/components/ArticleList/index.tsx
@@ -1,11 +1,14 @@
+import data from "./articles.json";
 import Article from "../Article";
 
 export default function ArticleList() {
   return (
-    <section className="articles mt-5 sm:mt-0 flex flex-col gap-5 m-auto max-w-5xl">
+    <section className="articles mt-5 mb-7 sm:mt-0 flex flex-col gap-5 m-auto sm:max-w-xl md:max-w-3xl lg:max-w-5xl">
       <h1 className="text-center text-3xl">Articles</h1>
       <div className="articles-list mt-5 sm:mt-0 grid gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        <Article />
+        {data.map((props, index) => (
+          <Article key={index} {...props} />
+        ))}
       </div>
     </section>
   );
