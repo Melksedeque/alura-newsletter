1b4278e70aaa4aa68828323e5b47614d85be32c1
Melksedeque <freelancer@melksedeque.com.br>
2025-03-09 15:06:37 -0300

    Criação de componente para o input do formulário

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,24 @@
+import { InputProps } from "../../types/Input";
+
+export default function Input({
+  type,
+  name,
+  value,
+  onChange,
+  placeholder,
+  disabled,
+  required,
+}: InputProps) {
+  return (
+    <input
+      type={type}
+      name={name}
+      className="bg-white dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full px-5 py-2 outline-none w-full"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      placeholder={placeholder}
+      disabled={disabled}
+      required={required}
+    />
+  );
+}
