04b8d69da5929f7997e53e75f71743ac750d1800
Melksedeque <freelancer@melksedeque.com.br>
2025-03-09 16:22:44 -0300

    Aplicação de transition no bg e de tamanho do logo com hover

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,11 @@ import { HeaderProps } from "../../types/Header";
 
 export default function Header({ user }: HeaderProps) {
   return (
-    <header className="header flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center p-5 sm:rounded-xl ">
+    <header className="header flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center p-5 sm:rounded-xl transition duration-300">
       <h2 className="text-gray-100">Bem-vindo, {user?.name || "Usuário"}!</h2>
-      <h1 className="header__title text-gray-100 text-xl">Alura Newsletter</h1>
+      <h1 className="header__title text-gray-100 text-xl hover:scale-110 cursor-pointer transition duration-300">
+        Alura Newsletter
+      </h1>
       <ToggleTheme />
     </header>
   );
