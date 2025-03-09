751ad764ca5a09067320c7110a1e9e524bb67b79
Melksedeque <freelancer@melksedeque.com.br>
2025-03-08 22:05:19 -0300

    Remoção de map de artigos do componente Lista para o Artigo individual

--- src/components/ArticleList/index.tsx
+++ src/components/ArticleList/index.tsx
@@ -1,21 +1,11 @@
-import data from "./articles.json";
+import Article from "../Article";
 
 export default function ArticleList() {
   return (
     <section className="articles mt-5 sm:mt-0 flex flex-col gap-5 m-auto max-w-5xl">
       <h1 className="text-center text-3xl">Articles</h1>
       <div className="articles-list mt-5 sm:mt-0 grid gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {data.map((article) => (
-          <div className="article" key={article.id}>
-            <h2>{article.title}</h2>
-            <p>{article.text}</p>
-            <ul>
-              {article.tags.map((tag) => (
-                <li key={tag}>{tag}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
+        <Article />
       </div>
     </section>
   );
