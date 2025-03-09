332d5f6905b361d362e7b12d70d6b044ef0a7de7
Melksedeque <freelancer@melksedeque.com.br>
2025-03-09 15:06:16 -0300

    Remoção de HTMLInput e uso de React.useState para tratar dos dados recebidos pelos inputs do form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,27 +1,45 @@
 // Usar typescript para validar os tipos dos campos e as variáveis
 
 import { FormProps } from "../../types/Form";
+import React from "react";
+import Input from "../Input";
 
 export default function Form({ onSubmit }: FormProps) {
+  const [formState, setFormState] = React.useState({
+    name: '',
+    email: ''
+  });
+
   const safeSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     e.stopPropagation();
-    const form = e.target as HTMLFormElement;
-    const nameInput = form[0] as HTMLInputElement;
-    const emailInput = form[1] as HTMLInputElement;
-    onSubmit(nameInput.value, emailInput.value);
+    onSubmit(formState.name, formState.email);
   };
 
   return (
     <form
       onSubmit={safeSubmit}
-      className="h-full flex flex-col items-center justify-center gap-10 -mt-20"
+      className="h-full flex flex-col items-center justify-center gap-10 -mt-20 mx-auto max-w-lg"
     >
-      <input type="text" name="name" placeholder="Insira seu Name" />
-      <input type="email" name="email" placeholder="Insira seu Email" />
+      <Input 
+        type="text" 
+        name="name" 
+        value={formState.name}
+        onChange={(value) => setFormState(prev => ({ ...prev, name: value }))}
+        placeholder="Insira seu Name" 
+        required 
+      />
+      <Input 
+        type="email" 
+        name="email" 
+        value={formState.email}
+        onChange={(value) => setFormState(prev => ({ ...prev, email: value }))}
+        placeholder="Insira seu Email" 
+        required 
+      />
       <button
         type="submit"
-        className="bg-alura-100 dark:bg-dark-200 cursor-pointer rounded-full text-gray-200 py-1 px-5 uppercase"
+        className="bg-alura-100 dark:bg-dark-200 cursor-pointer rounded-full text-gray-200 py-2 px-5 uppercase outline-none w-xs"
       >
         Enviar
       </button>
