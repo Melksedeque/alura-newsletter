fd6a3f581a3e5455f97f0388fd82720b34fdb98f
Melksedeque <freelancer@melksedeque.com.br>
2025-03-09 13:45:35 -0300

    Tipagem dos campos do formulário com HTMLFormElement e HTMLInputElement

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,12 +1,17 @@
-export default function Form({ onSubmit }) {
-  const safeSubmit = (e) => {
+// Usar typescript para validar os tipos dos campos e as variáveis
+
+import { FormProps } from '../../types/Form';
+
+export default function Form({ onSubmit }: FormProps) {
+  const safeSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     e.stopPropagation();
-    const name = e.target[0].value;
-    const email = e.target[1].value;
-    console.log(name, email);
-    onSubmit(name, email);
+    const form = e.target as HTMLFormElement;
+    const nameInput = form[0] as HTMLInputElement;
+    const emailInput = form[1] as HTMLInputElement;
+    onSubmit(nameInput.value, emailInput.value);
   };
+
   return (
     <form onSubmit={safeSubmit}>
       <input type="text" name="name" placeholder="Insira seu Name" />
