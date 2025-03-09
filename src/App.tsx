00af105a4173932c140ddf73f747d405bd055a14
Melksedeque <freelancer@melksedeque.com.br>
2025-03-09 13:11:59 -0300

    Chamada de componente Form; Verificação de usuário para exibir o form ou os artigos;

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import ArticleList from "./components/ArticleList";
 import Header from "./components/Header";
+import Form from "./components/Form";
 
 function App() {
+  const [user, setUser] = useState();
+  const hasUser = Boolean(user);
   return (
     <>
       <Header />
-      <ArticleList />
+      {hasUser && <ArticleList />}
+      {!hasUser && <Form onSubmit={setUser} />}
     </>
   );
 }
