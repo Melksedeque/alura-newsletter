f4f3aa210061e6145bce7ed0246e7406cbba0c80
Melksedeque <freelancer@melksedeque.com.br>
2025-03-09 14:07:51 -0300

    Condicional para exibir nome do usuário logado ou Usuário em caso de não llogado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,11 @@
 import ToggleTheme from "../ToggleTheme";
 
-import { HeaderProps } from '../../types/Header';
+import { HeaderProps } from "../../types/Header";
 
 export default function Header({ user }: HeaderProps) {
-  console.log(user);
   return (
     <header className="header flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center p-5 sm:rounded-xl sm:m-5 ">
-      <h2 className="text-gray-100">
-        Bem-vindo, {user ? user.name : "Usuário"}!
-      </h2>
+      <h2 className="text-gray-100">Bem-vindo, {user?.name || "Usuário"}!</h2>
       <h1 className="header__title text-gray-100 text-xl">Alura Newsletter</h1>
       <ToggleTheme />
     </header>
