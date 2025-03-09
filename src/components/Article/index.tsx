eb54a6c96bbaeb4ae3dab42059ef2e52bff5b434
Melksedeque <freelancer@melksedeque.com.br>
2025-03-09 16:23:05 -0300

    Aplicação de transition nas tags e nos cards article

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -1,8 +1,14 @@
-import { ArticleProps } from '../../types/Article';
+import { ArticleProps } from "../../types/Article";
 
-export default function Article({ title, text, tags, image, alt }: ArticleProps) {
+export default function Article({
+  title,
+  text,
+  tags,
+  image,
+  alt,
+}: ArticleProps) {
   return (
-    <article className="article flex flex-col items-center gap-2 p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-2xl sm:shadow-lg">
+    <article className="article flex flex-col items-center gap-2 p-5 transition duration-300 bg-gray-200 dark:bg-dark-200 sm:rounded-2xl sm:shadow-lg sm:dark:shadow-gray-950 sm:hover:shadow-gray-200 sm:hover:shadow-lg sm:dark:hover:shadow-none">
       <h2 className="text-xl text-alura-200 dark:text-gray-200 font-bold">
         {title}
       </h2>
@@ -10,7 +16,7 @@ export default function Article({ title, text, tags, image, alt }: ArticleProps)
         {tags.map((tag) => (
           <li
             key={tag}
-            className="bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 font-bold text-xs uppercase"
+            className="bg-alura-100 dark:bg-dark-100 cursor-pointer px-4 py-1 transition duration-300 rounded-full text-gray-200 font-bold text-xs uppercase hover:bg-alura-200 hover:scale-110"
           >
             {tag}
           </li>
