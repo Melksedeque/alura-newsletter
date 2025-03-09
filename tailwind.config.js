fdf01f5062d7778d8fefabd27c1d8d784acdbc88
Melksedeque <freelancer@melksedeque.com.br>
2025-03-08 21:10:06 -0300

    Criação de arquivo de configuração do Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    colors: {
+      dark: {
+        100: "rgb(18, 18, 18)",
+        200: "rgb(51, 51, 51)",
+      },
+      alura: {
+        100: "#167BF7",
+        200: "#051933",
+      },
+    },
+  },
+  plugins: [],
+};
