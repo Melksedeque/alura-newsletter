86110e8b8ec5ff0bda16a981856aff6f6bcbc0ef
Melksedeque <freelancer@melksedeque.com.br>
2025-03-08 22:02:57 -0300

    Estruturação de lista de artigos

--- src/components/ArticleList/index.tsx
+++ src/components/ArticleList/index.tsx
@@ -2,9 +2,9 @@ import data from "./articles.json";
 
 export default function ArticleList() {
   return (
-    <div className="articles">
-      <h1>Articles</h1>
-      <div className="articles-list">
+    <section className="articles mt-5 sm:mt-0 flex flex-col gap-5 m-auto max-w-5xl">
+      <h1 className="text-center text-3xl">Articles</h1>
+      <div className="articles-list mt-5 sm:mt-0 grid gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {data.map((article) => (
           <div className="article" key={article.id}>
             <h2>{article.title}</h2>
@@ -17,6 +17,6 @@ export default function ArticleList() {
           </div>
         ))}
       </div>
-    </div>
+    </section>
   );
 }
