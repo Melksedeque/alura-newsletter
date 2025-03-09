7e35f548ca9a411e2ee11d7e211ffe76b3220455
Melksedeque <freelancer@melksedeque.com.br>
2025-03-08 21:40:34 -0300

    Uso de classes customizadas no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import ToggleTheme from "../ToggleTheme";
 
 export default function Header() {
   return (
-    <header className="header flex h-20 bg-gray-500 justify-between items-center p-5 sm:rounded-xl sm:m-5 ">
+    <header className="header flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center p-5 sm:rounded-xl sm:m-5 ">
       <h2 className="text-gray-100">Bem-vindo, usuário!</h2>
       <h1 className="header__title text-gray-100 text-xl">Alura Newsletter</h1>
       <ToggleTheme />
