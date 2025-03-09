508e98d59ae3bccb77da954ca2f99ad6af0cd438
Melksedeque <freelancer@melksedeque.com.br>
2025-03-09 15:10:36 -0300

    Classes de hover no botão de submit

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -6,8 +6,8 @@ import Input from "../Input";
 
 export default function Form({ onSubmit }: FormProps) {
   const [formState, setFormState] = React.useState({
-    name: '',
-    email: ''
+    name: "",
+    email: "",
   });
 
   const safeSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -21,25 +21,27 @@ export default function Form({ onSubmit }: FormProps) {
       onSubmit={safeSubmit}
       className="h-full flex flex-col items-center justify-center gap-10 -mt-20 mx-auto max-w-lg"
     >
-      <Input 
-        type="text" 
-        name="name" 
+      <Input
+        type="text"
+        name="name"
         value={formState.name}
-        onChange={(value) => setFormState(prev => ({ ...prev, name: value }))}
-        placeholder="Insira seu Name" 
-        required 
+        onChange={(value) => setFormState((prev) => ({ ...prev, name: value }))}
+        placeholder="Insira seu Name"
+        required
       />
-      <Input 
-        type="email" 
-        name="email" 
+      <Input
+        type="email"
+        name="email"
         value={formState.email}
-        onChange={(value) => setFormState(prev => ({ ...prev, email: value }))}
-        placeholder="Insira seu Email" 
-        required 
+        onChange={(value) =>
+          setFormState((prev) => ({ ...prev, email: value }))
+        }
+        placeholder="Insira seu Email"
+        required
       />
       <button
         type="submit"
-        className="bg-alura-100 dark:bg-dark-200 cursor-pointer rounded-full text-gray-200 py-2 px-5 uppercase outline-none w-xs"
+        className="bg-alura-100 dark:bg-dark-200 cursor-pointer rounded-full text-gray-200 py-2 px-5 uppercase outline-none w-xs hover:animate-pulse hover:shadow-sm hover:shadow-gray-500 hover:dark:shadow-black"
       >
         Enviar
       </button>
